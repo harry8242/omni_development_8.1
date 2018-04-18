CyanogenMod/android_hardware_qcom_display

Silence log spam.
E/qdutils: int qdutils::getHDMINode(): Failed to open fb node 2
E/qdutils: int qdutils::getHDMINode(): Failed to open fb node 3
E/qdutils: int qdutils::getHDMINode(): Failed to find HDMI node

Change-Id: Ia26f26b79f1709da9991ccffa1583fca54bdaa5a

libqdutils/qd_utils.cpp
@@ -53,14 +53,14 @@ int getHDMINode(void)
             }
             fclose(displayDeviceFP);
         } else {
-            ALOGE("%s: Failed to open fb node %d", __func__, j);
+            ALOGVV("%s: Failed to open fb node %d", __func__, j);
         }
     }
 
     if (j < HWC_NUM_DISPLAY_TYPES)
         return j;
     else
-        ALOGE("%s: Failed to find HDMI node", __func__);
+        ALOGVV("%s: Failed to find HDMI node", __func__);
 
     return -1;
 }

harry8242/omni_development_8.1

libqdutils/qd_utils.cpp
@@ -53,14 +53,14 @@ int getHDMINode(void)
             }
             fclose(displayDeviceFP);
         } else {
-            ALOGE("%s: Failed to open fb node %d", __func__, j);
+            ALOGV("%s: Failed to open fb node %d", __func__, j);
         }
     }
 
     if (j < HWC_NUM_DISPLAY_TYPES)
         return j;
     else
-        ALOGE("%s: Failed to find HDMI node", __func__);
+        ALOGV("%s: Failed to find HDMI node", __func__);
 
     return -1;
 }