harry8242/android_build_soong

[2/2]soong: add TARGET_HAS_LEGACY_CAMERA_HAL1 to config

android/config.go
@@ -572,3 +572,8 @@ func (c *deviceConfig) TargetHeaderPathDir() string {
 func (c *deviceConfig) LegacyCam() bool {
 	return Bool(c.config.ProductVariables.LegacyCam)
 }
+
+func (c *deviceConfig) HasLegacyCameraHal1() bool {
+	return Bool(c.config.ProductVariables.HasLegacyCameraHal1)
+}
+

android/variable.go
@@ -176,6 +176,8 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	LegacyCam  *bool `json:",omitempty"`
+
+	HasLegacyCameraHal1  *bool `json:",omitempty"`
 }
 
 func boolPtr(v bool) *bool {

harry8242/omni_development_8.1

android/config.go
@@ -572,3 +572,7 @@ func (c *deviceConfig) TargetHeaderPathDir() string {
 func (c *deviceConfig) LegacyCam() bool {
 	return Bool(c.config.ProductVariables.LegacyCam)
 }
+
+func (c *deviceConfig) HasLegacyCamHal1() bool {
+	return Bool(c.config.ProductVariables.HasLegacyCamHal1)
+}

android/variable.go
@@ -176,6 +176,9 @@ type productVariables struct {
 	DeviceKernelHeaders []string `json:",omitempty"`
 
 	LegacyCam  *bool `json:",omitempty"`
+
+	HasLegacyCamHal1  *bool `json:",omitempty"`
+
 }
 
 func boolPtr(v bool) *bool {